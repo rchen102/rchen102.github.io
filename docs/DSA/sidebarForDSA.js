76509f8820367c607235d6685637d8ca906e26e2
rchen102 <rchen102@binghamton.edu>
2020-01-21 22:20:01 -0500

    DSA: Array

--- docs/DSA/sidebarForDSA.js
+++ docs/DSA/sidebarForDSA.js
@@ -3,10 +3,10 @@ module.exports = [
 	  title:'数据结构',
 	  collapsable: true,
 	  children:[
-		'/DSA/notes/1 Array',
-		'/DSA/notes/2 LinkedList',
-		'/DSA/notes/3 Stack',
-		'/DSA/notes/4 Queue'
+		'/DSA/DataStructure/1 Array',
+		'/DSA/DataStructure/2 LinkedList',
+		'/DSA/DataStructure/3 Stack',
+		'/DSA/DataStructure/4 Queue'
 	  ]
 	}, 
 	{
