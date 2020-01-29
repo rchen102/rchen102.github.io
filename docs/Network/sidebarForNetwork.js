342837650c3df85edd377be682ba1c3559d76f74
rchen102 <rchen102@binghamton.edu>
2020-01-28 20:44:22 -0500

    Network

--- docs/Network/sidebarForNetwork.js
+++ docs/Network/sidebarForNetwork.js
@@ -1,12 +1,21 @@
 module.exports = [
 	{
-	  title:'计算机网络',
+	  title:'计算机网络和因特网',
 	  collapsable: true,
 	  sidebarDepth: 1,
 	  children:[
 		'/Network/notes/计算机网络概述',
+	  ]
+	},
+	{
+	  title:'应用层',
+	  collapsable: false,
+	  sidebarDepth: 1,
+	  children:[
 		'/Network/notes/应用层1',
 		'/Network/notes/应用层2',
+		'/Network/notes/应用层3',
+		'/Network/notes/应用层4',
 	  ]
 	},
 ]
